e63bdd04e5e5ccae585c77efd7a36625c40ecfd0
Ade Johnson <a_jaws1408@hotmail.com>
2025-02-09 19:40:12 +0000

    Update secrets.tf

--- terraform.tf/secrets.tf
+++ terraform.tf/secrets.tf
@@ -6,3 +6,19 @@ resource "aws_secretsmanager_secret_version" "db_password_value" {
   secret_id     = aws_secretsmanager_secret.db_password.id
   secret_string = "London25"
 }
+
+resource "aws_secretsmanager_secret" "db_secrets" {
+  name        = "interstellar-db-creds"
+  description = "Database credentials for Interstellar Route Planner"
+}
+
+resource "aws_secretsmanager_secret_version" "db_secrets_version" {
+  secret_id = aws_secretsmanager_secret.db_secrets.id
+  secret_string = jsonencode({
+    DB_NAME     = "interstellar_db"
+    DB_USER     = "admin"
+    DB_PASSWORD = "London25"
+    DB_HOST     = "postgres-container"
+    DB_PORT     = "5432"
+  })
+}
